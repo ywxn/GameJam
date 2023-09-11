5678d5233691697fdc92e05fc7d45d6ca7686db9
neel <troegern@whitman.edu>
2023-09-11 16:04:51 +0200

    commit

--- Assets/Scripts/SlimeController.cs
+++ Assets/Scripts/SlimeController.cs
@@ -1,18 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Tilemaps;
 
 public class SlimeController : MonoBehaviour
+
 {
+    public Sprite[] slimeIdle;
+    //publci Sprite[] stimeJump;
+
+    SpriteRenderer spriteRenderer;
+    public float framesPerSecond = 15;
+
+    int currentFrameIndex = 0;
+    float frameTimer;
+
+    private Rigidbody2D rb;
+
+    public float speed = 1f; 
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 vel = rb.velocity;
+        vel.x = -speed;
+        rb.velocity = vel;
+        frameTimer += Time.deltaTime;
+        Animate(slimeIdle);
+    }
+    void Animate(Sprite[] frames)
+    {
+       //if (frames.Length == 0)
+       //{
+      // Ensure there are frames to animate.
+       //    return;
+       //}
+
+       if (frameTimer >= (1f / framesPerSecond))
+       {
+          frameTimer = 0;
+          currentFrameIndex = (currentFrameIndex + 1) % frames.Length;
+          // Update the sprite and flipX property based on the current frame.
+          spriteRenderer.sprite = frames[currentFrameIndex];
+       }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 }
---
+++ Assets/Scripts/SlimeSpawner.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeSpawner : MonoBehaviour
+
+{
+    float timer;
+    public GameObject slime;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0) {
+            Instantiate(slime, transform.position, transform.rotation);
+            timer = Random.Range(0, 1);
+        }
+    }
+}
