e656fbb91a09d2eb65ddcb3d06c2404fe447f9e6
neel <troegern@whitman.edu>
2023-09-11 14:35:57 +0200

    want to change animations for jump up/down

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class PlayerAnimation : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public Sprite[] run;
-    public Sprite[] jump;
+    public Sprite[] jumpUp;
+    public Sprite[] jumpDown;
 
     SpriteRenderer spriteRenderer;
 
@@ -15,21 +17,36 @@ public class PlayerAnimation : MonoBehaviour
     int currentFrameIndex = 0;
     float frameTimer;
 
+    public float jumpForce = 10f; // Adjust this value to control the jump height
+    private Rigidbody2D rb;
+    private bool canJump = true;
+
+
     // Start is called before the first frame update
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         frameTimer = 1f / framesPerSecond;
         currentFrameIndex = 0;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         frameTimer += Time.deltaTime;
-        if (Input.GetKey(KeyCode.Space))
+        if (canJump && Input.GetKeyDown(KeyCode.Space))
         {
-            Animate(jump);
+            Jump();
+            canJump = false;
+        }
+        if (rb.velocity.y > 0)
+        {
+            Animate(jumpUp);
+        }
+        if (rb.velocity.y < 0)
+        {
+            Animate(jumpDown);
         }
         else
         {
@@ -38,21 +55,34 @@ public class PlayerAnimation : MonoBehaviour
 
         void Animate(Sprite[] frames)
         {
-            if (frames.Length == 0)
-            {
+            //if (frames.Length == 0)
+            //{
                 // Ensure there are frames to animate.
-                return;
-            }
+            //    return;
+            //}
 
             if (frameTimer >= (1f / framesPerSecond))
             {
                 frameTimer = 0;
                 currentFrameIndex = (currentFrameIndex + 1) % frames.Length;
-
                 // Update the sprite and flipX property based on the current frame.
                 spriteRenderer.sprite = frames[currentFrameIndex];
             }
         }
     }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        canJump = false;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            canJump = true;
+        }
+    }
+
 }
 
